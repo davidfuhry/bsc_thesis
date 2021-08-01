6e7f2f8d19c3937ec300865f107cfe80cc511813
David Fuhry <df15nocu@studserv.uni-leipzig.de>
2021-08-01 15:17:27 +0200

    Add vorgehen and datapreprocessing sections

--- make_graphics.R
+++ make_graphics.R
@@ -16,6 +16,7 @@ stats <- corpus %>%
     group_by(site) %>%
     add_tally() %>%
     summarise(start_date = min(date), end_date = max(date), avg_length = mean(nchar), n_articles = n_distinct(url)) %>%
+    mutate(pub_amount = round(n_articles / (interval(start_date, end_date) %/% months(1)), digits = 1)) %>%
     mutate(avg_length = round(avg_length)) %>%
     mutate(start_date = format(start_date, "%d.%m.%Y")) %>%
     mutate(end_date = format(end_date, "%d.%m.%Y")) %>%
@@ -25,8 +26,25 @@ stats <- corpus %>%
 colnames(stats) <- stats[1, ]
 stats <- stats[-1, ]
 
-stats <- stats %>%
-    mutate()
+kableExtra::kbl(t(stats), format = "latex", booktabs = TRUE)
+
+### Stats for comparitive Corpus
+
+stats <- corpus %>%
+    filter(is_conspiracy == FALSE) %>%
+    mutate(nchar = nchar(text)) %>%
+    group_by(site) %>%
+    add_tally() %>%
+    summarise(start_date = min(date, na.rm = TRUE), end_date = max(date, na.rm = TRUE), avg_length = mean(nchar), n_articles = n_distinct(url)) %>%
+    mutate(pub_amount = round(n_articles / (interval(start_date, end_date) %/% months(1)), digits = 1)) %>%
+    mutate(avg_length = round(avg_length)) %>%
+    mutate(start_date = format(start_date, "%d.%m.%Y")) %>%
+    mutate(end_date = format(end_date, "%d.%m.%Y")) %>%
+    t %>%
+    data.frame
+
+colnames(stats) <- stats[1, ]
+stats <- stats[-1, ]
 
 kableExtra::kbl(t(stats), format = "latex", booktabs = TRUE)
 
